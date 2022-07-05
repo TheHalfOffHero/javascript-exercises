5c7e9bc175158323a484fd5992eb59b463ac32b4
Matt Ruiz <mfruiz33@gmail.com>
2022-07-05 19:38:07 -0400

    finished -2_repeatString excercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let result = "";
 
+    //if num is a negative number then we return an error else proceed
+    if (num < 0) {
+        return "ERROR"
+    }
+
+    for (let index = 0; index < num; index++) {
+        result = result + str
+    }
+    return result
 };
 
 // Do not edit below this line
