c04a39faf4bfc9264a88daf6a9b22a1bd0dd73dd
Matt Ruiz <mfruiz33@gmail.com>
2022-07-05 20:54:51 -0400

    started work on 04_removeFromrray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(...arr) {
+    let result = arr[0]
 
+    for (let i = 1; i < arr.length; i++) {
+        result = result.filter(x => x != arr[i])
+    }
+    return result
 };
 
 // Do not edit below this line
