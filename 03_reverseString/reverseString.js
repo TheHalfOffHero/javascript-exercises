8906e8e4ac4e0930c40ad1220a50a1b739fae53f
Matt Ruiz <mfruiz33@gmail.com>
2022-07-05 20:05:29 -0400

    completed 03_reverseString excercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let tmp = str.split("");
+    tmp = tmp.reverse();
+    tmp = tmp.join("");
+    return tmp;
 };
 
 // Do not edit below this line
